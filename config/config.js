cca9e2a9e19d4c54b534ce5875c3408a1b6c8801
Bruce Harms <bruceh_88@comcast.net>
2019-01-21 19:03:05 -0500

    prod db correction

--- config/config.js
+++ config/config.js
@@ -14,10 +14,7 @@ module.exports = {
     dialect: 'mysql'
   },
   production: {
-    username: process.env.PROD_DB_USERNAME,
-    password: process.env.PROD_DB_PASSWORD,
-    database: process.env.PROD_DB_NAME,
-    host: process.env.PROD_DB_HOSTNAME,
-    dialect: 'mysql'
+    use_env_variable: "JAWSDB_URL",
+    dialect: "mysql"
   }
 }
